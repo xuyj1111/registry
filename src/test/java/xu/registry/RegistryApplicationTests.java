10b2462e08949cb22abc2144799551daf799feea
yujun.xu <yujun.xu@linkflowtech.com>
2022-03-02 16:54:54 +0800

    eureka server

---
+++ src/test/java/xu/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package xu.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
